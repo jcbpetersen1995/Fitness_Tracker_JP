bf6a56ec7880f6c1bab2617e1aab9c9fcc237934
Jacob Petersen <jcb.petersen1995@gmail.com>
2020-04-14 10:54:00 -0600

    fitness app working as designed

---
+++ models/workouts.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Enter a type of exercise."
+            },
+            name: {
+                type: String,
+                required: "Enter the name of exercise."
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+        }
+    ]
+});
+// {
+//     day: new Date().setDate(new Date().getDate()-10),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bicep Curl",
+//         duration: 20,
+//         weight: 100,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/api.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        req.params.id,
+        {$push: {exercises: req.body}}
+    )
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
